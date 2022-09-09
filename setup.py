1cc89e2d65587b4ef68db479120d237779f119dc
Tom Silver <tomssilver@gmail.com>
2022-09-09 14:11:39 -0400

    create setup script

---
+++ setup.py
@@ -0,0 +1,9 @@
+"""Setup script."""
+from setuptools import find_packages, setup
+
+setup(
+    name="lisdf_models",
+    version="0.0.0",
+    packages=find_packages(include=["lisdf_models"]),
+    include_package_data=True,
+)
