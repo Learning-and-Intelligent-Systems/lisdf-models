f72c765b166c375ec69ff4950c2fcc81a4f88ceb
Tom Silver <tomssilver@gmail.com>
2022-09-09 16:35:42 -0400

    add py.typed to setup.py

--- setup.py
+++ setup.py
@@ -5,5 +5,6 @@ setup(
     name="lisdf_models",
     version="0.0.0",
     packages=find_packages(include=["lisdf_models"]),
+    package_data={"lisdf_models": ["py.typed"]},  # for mypy
     include_package_data=True,
 )
